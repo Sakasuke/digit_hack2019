965929a016e79d6d0e7b322b5fea0945ad65e95a
Ryosuke Sakaguchi <sk.ryosuke@gmail.com>
2019-06-12 00:42:32 +0900

    ctl

--- liff-app/liff.js
+++ liff-app/liff.js
@@ -38,7 +38,7 @@ function handlerToggleLed() {
 
 function uiToggleLedButton(state) {
     const el = document.getElementById("btn-led-toggle");
-    el.innerText = state ? "Switch LED OFF" : "Switch LED ON";
+    el.innerText = state ? "OFF" : "LED";
 
     if (state) {
         el.classList.add("led-on");
